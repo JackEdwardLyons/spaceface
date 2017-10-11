b750d3de714f6d87183b906d2a1bbc2f22aa6aa3
Jack Lyons <jack.edward.lyons@gmail.com>
2017-10-11 16:51:49 +0800

    Revert back to previous scroll functionality

--- assets/spaceface/js/custom-scripts.js
+++ assets/spaceface/js/custom-scripts.js
@@ -28,12 +28,12 @@
   function addModal(person) {
 
     // Get the modal elements 
-    var modal         = $('#modal' + person);
-    var btn           = $(".js-show" + person);
-    var nav           = $('.navbar-fixed-top');
-    var close         = $('.close-modal');
-    var footer        = $('#footer1-9');
-    var linkText      = $('.modal-link__text');
+    var modal = $('#modal' + person);
+    var btn = $(".js-show" + person);
+    var nav = $('.navbar-fixed-top');
+    var close = $('.close-modal');
+    var footer = $('#footer1-9');
+    var linkText = $('.modal-link__text');
     var hideOnOverlay = $('.js-hide-' + person);
 
     // When the user clicks on the button, open the modal 
@@ -71,15 +71,23 @@
    * --------------------- */
   (function () {
 
-    zoomEffect('.js-zoom-1', 700, 1380, 15);
-    zoomEffect('.js-zoom-2', 2000, 2550, 20);
-    zoomEffect('.js-zoom-3', 3100, 3700, 30);
+    if ( $(window).width() > 1600 ) {
+      zoomEffect('.js-zoom-1', 700, 1380, 15);
+      zoomEffect('.js-zoom-2', 2900, 4550, 45);
+      zoomEffect('.js-zoom-3', 5100, 6700, 70);
+    } else {
+      zoomEffect('.js-zoom-1', 700, 1380, 15);
+      zoomEffect('.js-zoom-2', 1500, 2500, 45);
+      zoomEffect('.js-zoom-3', 2600, 3700, 60);
+    }
+    
 
     function zoomEffect(element, docMin, docMax, screenDivide) {
 
       var previousScroll = 0;
       var zoomFont = $(element);
       var docWidth = $(document).width();
+
 
       $(window).scroll(function () {
 
@@ -88,32 +96,32 @@
 
         console.log('scroll: ', currentScroll);
 
+        function checkFontSize() {
+
+          function correctHeadingSize(multiplerSize, fontSize) {
+            if (multiplier < multiplerSize) {
+              zoomFont.css({
+                'font-size': (multiplier) + 'px'
+              });
+            } else {
+              zoomFont.css({
+                'font-size': fontSize
+              });
+            }
+          } // end correctHeadingSize()
+
+          if (docWidth > 1300) {
+            correctHeadingSize(120, '80px');
+          } else if (docWidth > 1100) {
+            correctHeadingSize(100, '80px');
+          }
+
+        } // end checkFontSize() 
+
         if (currentScroll > docMin && currentScroll < docMax) {
 
           zoomFont.css('font-size', '40px');
           multiplier = (currentScroll / screenDivide);
-
-          function checkFontSize() {
-
-            function correctHeadingSize(multiplerSize, fontSize) {
-              if (multiplier < multiplerSize) {
-                zoomFont.css({
-                  'font-size': (multiplier) + 'px'
-                });
-              } else {
-                zoomFont.css({
-                  'font-size': fontSize
-                });
-              }
-            } // end correctHeadingSize()
-
-            if (docWidth > 1300) {
-              correctHeadingSize(120, '80px');
-            } else if (docWidth > 1100) {
-              correctHeadingSize(100, '80px');
-            }
-
-          } // end checkFontSize() 
 
           if (currentScroll > previousScroll) {
             // going down
@@ -136,16 +144,16 @@
 
     /* Scrolling */
     $(window).scroll(function () {
-      var $maxScroll = 200; 
-      var $maxScale  = 2;
-      var $scroll    = $(window).scrollTop();
-      var $x         = $scroll / 250 + 1;
+      var $maxScroll = 200;
+      var $maxScale = 2;
+      var $scroll = $(window).scrollTop();
+      var $x = $scroll / 250 + 1;
 
-      if ( $scroll > $maxScroll) $x = $maxScale;
+      if ($scroll > $maxScroll) $x = $maxScale;
 
-      if ( $(window).width() > 1000 && $scroll ) {
+      if ($(window).width() > 1000 && $scroll) {
         $('.hero-image').css({
-          transform: 'scale(' + ( $x ) + ')'
+          transform: 'scale(' + ($x) + ')'
         });
       } else {
         $('.hero-image').css({
@@ -163,13 +171,15 @@
    * Srcoll Fix 
    * ----------- */
 
-  $(document).ready(function() {
-    $('a[href^="#"]').click(function() {
-        var target = $(this.hash);
-        if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
-        if (target.length == 0) target = $('html');
-        $('html, body').animate({ scrollTop: target.offset().top - 100 }, 1000);
-        return false;
+  $(document).ready(function () {
+    $('a[href^="#"]').click(function () {
+      var target = $(this.hash);
+      if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+      if (target.length == 0) target = $('html');
+      $('html, body').animate({
+        scrollTop: target.offset().top - 100
+      }, 1000);
+      return false;
     });
   });
 
